133b23028129dcb4490fe3e1995357561de9483e
NasKar2 <jschwrtz9@gmail.com>
2018-07-14 14:23:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# plex-freenas-iocage
+
+Script to create an iocage jail on Freenas 11.1U4 from scratch with plex.
+
+All apps will be placed in a jail with separate data directory to allow for easy reinstallation/backup.
+
+Plex will be installed with the default user/group (plex/plex) and the media group will include plex to allow reading of the media files.
+
+Thanks to Pentaflake for his work on installing these apps in an iocage jail.
+
+https://forums.freenas.org/index.php?resources/fn11-1-iocage-jails-plex-tautulli-sonarr-radarr-lidarr-jackett-ombi-transmission-organizr.58/
+
+### Prerequisites
+Edit file plex-config
+
+Edit plex-config file with your network information and directory data name you want to use and location of your media files and torrents.
+
+PLEX_DATA="plexpass" will create a data directory /mnt/v1/apps/plexpass to store all the data for that app.
+
+MEDIA_LOCATION will set the location of your media files, in this example /mnt/v1/media
+
+TORRENTS_LOCATION will set the location of your torrent files, in this example /mnt/v1/torrents
+
+PLEX_TYPE needs to be set to plexpass or plex depending on which version you want.
+
+
+```
+JAIL_IP="192.168.5.57"
+DEFAULT_GW_IP="192.168.5.1"
+INTERFACE="igb0"
+VNET="off"
+POOL_PATH="/mnt/v1"
+JAIL_NAME="plexapps"
+PLEX_DATA="plexdata2"
+MEDIA_LOCATION="media"
+TORRENTS_LOCATION="torrents"
+PLEX_TYPE="plexpass"
+```
+## Install Plex in fresh Jail
+
+Create an iocage jail to install plex.
+
+Then run this command
+```
+./plexinstallplex.sh
+```
---
+++ plexinstallplex.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+# Build an iocage jail under FreeNAS 11.1 with Plex
+# https://github.com/NasKar2/plex-freenas-iocage
+
+# Check for root privileges
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run with root privileges"
+   exit 1
+fi
+
+# Initialize defaults
+JAIL_IP=""
+DEFAULT_GW_IP=""
+INTERFACE=""
+VNET="off"
+POOL_PATH=""
+JAIL_NAME="mono"
+PLEX_DATA=""
+MEDIA_LOCATION=""
+TORRENTS_LOCATION=""
+PLEX_TYPE="plexpass"
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+. $SCRIPTPATH/plex-config
+CONFIGS_PATH=$SCRIPTPATH/configs
+
+# Check for plex-config and set configuration
+if ! [ -e $SCRIPTPATH/plex-config ]; then
+  echo "$SCRIPTPATH/plex-config must exist."
+  exit 1
+fi
+
+# Check that necessary variables were set by plex-config
+if [ -z $JAIL_IP ]; then
+  echo 'Configuration error: JAIL_IP must be set'
+  exit 1
+fi
+if [ -z $DEFAULT_GW_IP ]; then
+  echo 'Configuration error: DEFAULT_GW_IP must be set'
+  exit 1
+fi
+if [ -z $INTERFACE ]; then
+  echo 'Configuration error: INTERFACE must be set'
+  exit 1
+fi
+if [ -z $POOL_PATH ]; then
+  echo 'Configuration error: POOL_PATH must be set'
+  exit 1
+fi
+
+if [ -z $JAIL_NAME ]; then
+  echo 'Configuration error: JAIL_NAME must be set'
+  exit 1
+fi
+
+
+#if [ -z $PLEX_DATA ]; then
+#  echo 'Configuration error: PLEX_DATA must be set'
+#  exit 1
+#fi
+
+if [ "$PLEX_TYPE" != "plex" ] && [ "$PLEX_TYPE" != "plexpass" ]; then
+  echo '${PLEX_TYPE} Configuration error: PLEX_DATA must be set to plex or plexpass'
+  echo ${PLEX_TYPE}
+  exit 1
+fi
+
+if [ -z $MEDIA_LOCATION ]; then
+  echo 'Configuration error: MEDIA_LOCATION must be set'
+  exit 1
+fi
+
+if [ -z $TORRENTS_LOCATION ]; then
+  echo 'Configuration error: TORRENTS_LOCATION must be set'
+  exit 1
+fi
+
+#
+# Create Jail
+echo '{"pkgs":["nano","ca_root_nss"]}' > /tmp/pkg.json
+iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r 11.1-RELEASE ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}"
+
+rm /tmp/pkg.json
+
+mkdir -p ${POOL_PATH}/apps/${PLEX_DATA}
+#mkdir -p ${POOL_PATH}/${MEDIA_LOCATION}
+plex_config=${POOL_PATH}/apps/${PLEX_DATA}
+iocage exec ${JAIL_NAME} 'sysrc ifconfig_epair0_name="epair0b"'
+
+iocage fstab -a ${JAIL_NAME} ${CONFIGS_PATH} /mnt/configs nullfs rw 0 0
+iocage fstab -a ${JAIL_NAME} ${POOL_PATH}/apps/ /config nullfs rw 0 0
+iocage fstab -a ${JAIL_NAME} ${POOL_PATH}/${MEDIA_LOCATION} /mnt/media nullfs rw 0 0
+
+iocage restart ${JAIL_NAME} 
+
+#
+# Make media the user of the jail and create group media and make media a user of the that group
+iocage exec ${JAIL_NAME} "pw user add media -c media -u 8675309  -d /nonexistent -s /usr/bin/nologin"
+iocage exec ${JAIL_NAME} "pw groupadd -n media -g 8675309"
+iocage exec ${JAIL_NAME} "pw groupmod media -m plex"
+
+#
+# Make pkg upgrade get the latest repo
+iocage exec ${JAIL_NAME} mkdir -p /usr/local/etc/pkg/repos/
+iocage exec ${JAIL_NAME} cp -f /mnt/configs/FreeBSD.conf /usr/local/etc/pkg/repos/FreeBSD.conf
+
+#
+# Upgrade to the lastest repo
+iocage exec ${JAIL_NAME} pkg upgrade -y
+iocage restart ${JAIL_NAME}
+
+# Install Plex
+iocage exec ${JAIL_NAME} chown -R plex:plex /config/${PLEX_DATA}
+if [ $PLEX_TYPE == "plexpass" ]; then
+   echo "plexpass to be installed"
+   iocage exec ${JAIL_NAME} pkg install -y plexmediaserver-plexpass
+   iocage exec ${JAIL_NAME} sysrc "plexmediaserver_plexpass_enable=YES"
+   iocage exec ${JAIL_NAME} sysrc plexmediaserver_plexpass_support_path="/config/${PLEX_DATA}"
+   iocage exec ${JAIL_NAME} sysrc plexmediaserver_plexpass_user="plex"
+   iocage exec ${JAIL_NAME} sysrc plexmediaserver_plexpass_group="plex"
+   iocage exec ${JAIL_NAME} service plexmediaserver_plexpass start
+else
+   echo "plex to be installed"
+   iocage exec ${JAIL_NAME} pkg install -y plexmediaserver
+   iocage exec ${JAIL_NAME} sysrc "plexmediaserver_enable=YES"
+   iocage exec ${JAIL_NAME} sysrc plexmediaserver_support_path="/config/${PLEX_DATA}"
+   iocage exec ${JAIL_NAME} sysrc plexmediaserver_user="plex"
+   iocage exec ${JAIL_NAME} sysrc plexmediaserver_group="plex"
+   iocage exec ${JAIL_NAME} service plexmediaserver start
+fi
+
+echo "${PLEX_TYPE} installed"
+
+#
+# remove /mnt/configs as no longer needed
+iocage fstab -r ${JAIL_NAME} ${CONFIGS_PATH} /mnt/configs nullfs rw 0 0
+
+echo
+echo "${PLEX_TYPE} should be available at http://${JAIL_IP}:32400/web/index.html"
+
